4a3eac203290824adc08c240a278227a91443653
toshiya-oota <toshiya.1988.ota@gmail.com>
2019-09-25 09:41:39 +0000

    bbs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,9 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (\App::environment('production')) {
-            \URL::forceScheme('https');
-        }
         
         Schema::defaultStringLength(191);
         \DB::listen(function ($query) {
